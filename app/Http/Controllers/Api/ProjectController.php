cc477256640993cd76b2b93d71eb3ea82bba52bd
niccoloruberti <nikiruberti@gmail.com>
2023-08-29 21:02:25 +0200

    aggiunta paginazione

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::with('technologies', 'type')->get();
+        $projects = Project::with('technologies', 'type')->paginate(6);
         return response()->json([
             'success' => true,
             'results' => $projects
